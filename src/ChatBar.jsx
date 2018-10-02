650ed3d5ff02009c6b857d343a81157067719b79
Angela <liny0207@gmail.com>
2018-10-02 05:46:54 +0000

    updates the ChatBar.jsx

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -3,12 +3,10 @@ import React, {Component} from 'react';
 class ChatBar extends Component {
   render() {
     return (
-    <div>
     <footer className="chatbar">
       <input className="chatbar-username" placeholder="Your Name (Optional)" />
       <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
     </footer>
-    </div>
     );
   }
 }
