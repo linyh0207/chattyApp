3997908b433a707af8742d1a0bf51b99618b7b0d
Angela <liny0207@gmail.com>
2018-10-05 05:34:12 +0000

    adds placeholder for username input field in chatbar

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -5,7 +5,7 @@ class ChatBar extends Component {
     
     return (
     <footer className="chatbar">
-      <input onKeyUp={this.props.handleNewUserName}className="chatbar-username" defaultValue={this.props.currentuser} />
+      <input onKeyUp={this.props.handleNewUserName}className="chatbar-username" defaultValue={this.props.currentuser} placeholder="Type a name and hit ENTER" />
       <input onKeyUp={this.props.handleKeyPress} className="chatbar-message" placeholder="Type a message and hit ENTER" />
     </footer>
     );
